67352053266131f52ec20a6a4cc5ed64ee101879
suhongkim98 <kshz9815@naver.com>
2021-09-25 15:12:35 +0900

    임의 메시지 전달 API 작성

---
+++ src/main/java/com/allknu/backend/core/service/FCMApiServiceInterface.java
@@ -0,0 +1,7 @@
+package com.allknu.backend.core.service;
+
+import com.allknu.backend.web.dto.RequestFCMMessage;
+
+public interface FCMApiServiceInterface {
+    void pushToKafkaWebMessage(RequestFCMMessage.Web message);
+}
---
+++ src/main/java/com/allknu/backend/provider/service/FCMApiService.java
@@ -0,0 +1,20 @@
+package com.allknu.backend.provider.service;
+
+import com.allknu.backend.core.service.FCMApiServiceInterface;
+import com.allknu.backend.kafka.MessageProducer;
+import com.allknu.backend.kafka.dto.FCMWebMessage;
+import com.allknu.backend.web.dto.RequestFCMMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FCMApiService implements FCMApiServiceInterface {
+    private final MessageProducer messageProducer; // FCM 마이크로서비스로 보내기 위한 카프카 프로듀서
+
+    @Override
+    public void pushToKafkaWebMessage(RequestFCMMessage.Web message) {
+        FCMWebMessage fcmWebMessage = message.toFCMRequestMessage();
+        messageProducer.sendFCMMessage(fcmWebMessage);
+    }
+}
