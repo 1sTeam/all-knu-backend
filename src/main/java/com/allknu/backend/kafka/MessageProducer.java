964c86934f130b03282ccf98f2a4816e57de75f6
suhongkim98 <kshz9815@naver.com>
2021-09-25 15:11:25 +0900

    DTO이름 변경 및 전달 멤버변수 변경

--- src/main/java/com/allknu/backend/kafka/MessageProducer.java
+++ src/main/java/com/allknu/backend/kafka/MessageProducer.java
@@ -1,6 +1,6 @@
 package com.allknu.backend.kafka;
 
-import com.allknu.backend.kafka.dto.FCMRequestMessage;
+import com.allknu.backend.kafka.dto.FCMWebMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -11,9 +11,9 @@ public class MessageProducer {
     private static final String FCM_TOPIC = "fcm";
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void sendFCMMessage(FCMRequestMessage fcmRequestMessage) {
+    public void sendFCMMessage(FCMWebMessage fcmWebMessage) {
 
         System.out.println(String.format("Produce message : success" ));
-        this.kafkaTemplate.send(FCM_TOPIC, fcmRequestMessage);
+        this.kafkaTemplate.send(FCM_TOPIC, fcmWebMessage);
     }
 }
