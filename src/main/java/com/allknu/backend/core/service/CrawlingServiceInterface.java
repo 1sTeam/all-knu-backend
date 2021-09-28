dbb219722c4117adf095fc79f30443f9125768d0
suhongkim98 <kshz9815@naver.com>
2021-09-29 00:09:23 +0900

    공지사항 크롤링에 조회수, 작성자, 작성일자, 유형, 게시글번호 추가

--- src/main/java/com/allknu/backend/core/service/CrawlingServiceInterface.java
+++ src/main/java/com/allknu/backend/core/service/CrawlingServiceInterface.java
@@ -1,10 +1,11 @@
 package com.allknu.backend.core.service;
 
+import com.allknu.backend.core.types.UnivNoticeType;
 import com.allknu.backend.web.dto.ResponseCrawling;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CrawlingServiceInterface {
-    Optional<List<ResponseCrawling.UnivNotice>> getUnivNotice(int pageNum);
+    Optional<List<ResponseCrawling.UnivNotice>> getUnivNotice(int pageNum, UnivNoticeType type);
 }
