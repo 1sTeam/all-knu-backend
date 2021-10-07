33f4ce5a525a5819dd5738b4f6ed7f081d8cb7d5
suhongkim98 <kshz9815@naver.com>
2021-10-07 13:43:25 +0900

    knu login 테스트코드 작성

--- src/test/java/com/allknu/backend/BackendApplicationTests.java
+++ src/test/java/com/allknu/backend/BackendApplicationTests.java
@@ -1,5 +1,8 @@
 package com.allknu.backend;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,7 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 @SpringBootTest
 @ActiveProfiles("local")
@@ -42,4 +47,33 @@ class BackendApplicationTests {
 
 	}
 
+	@Test
+	void loginKnuTest() {
+		String url = "***REMOVED***?user_id=20171234&user_pwd=1234";
+		try {
+
+			Connection.Response res = Jsoup.connect(url)
+					.method(Connection.Method.GET)
+					.ignoreContentType(true)
+					.userAgent("***REMOVED***")
+					.execute();
+
+			System.out.println(res.body());
+			ObjectMapper mapper = new ObjectMapper();
+			JsonNode jsonNode = mapper.readTree(res.body()); // json mapper
+
+			if(jsonNode.get("result").equals("success")) {
+				System.out.println("로그인 성공");
+				Map<String, String> cookies = res.cookies();
+
+				for( Map.Entry<String, String> elem : cookies.entrySet() ){
+					System.out.println( String.format("키 : %s, 값 : %s", elem.getKey(), elem.getValue()) );
+				}
+			} else {
+				System.out.println("로그인 실패");
+			}
+ 		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
 }
