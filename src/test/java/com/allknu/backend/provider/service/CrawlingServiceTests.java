3e4c8ded8c95a1eb2b38c57e84b9429ca67a0ecb
suhongkim98 <kshz9815@naver.com>
2021-10-10 16:22:34 +0900

    모든 학과 공지사항 조회 테스트 코드 작성

--- src/test/java/com/allknu/backend/provider/service/CrawlingServiceTests.java
+++ src/test/java/com/allknu/backend/provider/service/CrawlingServiceTests.java
@@ -1,6 +1,7 @@
 package com.allknu.backend.provider.service;
 
 
+import com.allknu.backend.core.types.MajorNoticeType;
 import com.allknu.backend.core.types.UnivNoticeType;
 import com.allknu.backend.web.dto.ResponseCrawling;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.EnumSet;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ActiveProfiles("local")
@@ -25,7 +29,25 @@ public class CrawlingServiceTests {
         List<ResponseCrawling.UnivNotice> notices = crawlingService.getUnivNotice(1, UnivNoticeType.ALL).orElseGet(()->null);
         for(int i = 0 ; i < notices.size() ; i++) {
             ResponseCrawling.UnivNotice notice = notices.get(i);
-            System.out.println(notice.getTitle() + notice.getLink() + notice.getDate() + notice.getViews());
+            System.out.println(notice.getTitle() + notice.getDate() + notice.getViews() + notice.getLink());
         }
+    }
+
+    @Test
+    @DisplayName("기본 강남대 템플릿을 사용하는 학과 공지사항 크롤링 테스트")
+    void getMajorNoticeTest() {
+        //EnumSet 이용해 모든 타입 단위테스트
+        EnumSet.allOf(MajorNoticeType.class)
+                .forEach(type -> {
+                    System.out.println(type.getKorean() + "시작하겠습니다!");
+                    List<ResponseCrawling.UnivNotice> notices = crawlingService.getMajorDefaultTemplateNotice(1, type).orElseGet(()->null);
+
+                    assertNotNull(notices);
+
+                    for(int i = 0 ; i < notices.size() ; i++) {
+                        ResponseCrawling.UnivNotice notice = notices.get(i);
+                        System.out.println(notice.getTitle() + notice.getDate() + notice.getViews() + notice.getLink());
+                    }
+                });
     }
 }
