173cfc3d799d7826e867f9c01afa05a4ed8f19a4
suhongkim98 <kshz9815@naver.com>
2021-10-10 16:22:23 +0900

    학과 공지사항 조회 API 제작

--- src/main/java/com/allknu/backend/core/service/CrawlingServiceInterface.java
+++ src/main/java/com/allknu/backend/core/service/CrawlingServiceInterface.java
@@ -1,5 +1,6 @@
 package com.allknu.backend.core.service;
 
+import com.allknu.backend.core.types.MajorNoticeType;
 import com.allknu.backend.core.types.UnivNoticeType;
 import com.allknu.backend.web.dto.ResponseCrawling;
 
@@ -8,4 +9,5 @@ import java.util.Optional;
 
 public interface CrawlingServiceInterface {
     Optional<List<ResponseCrawling.UnivNotice>> getUnivNotice(int pageNum, UnivNoticeType type);
+    Optional<List<ResponseCrawling.UnivNotice>> getMajorDefaultTemplateNotice(int pageNum, MajorNoticeType type);
 }
--- src/main/java/com/allknu/backend/web/CrawlingController.java
+++ src/main/java/com/allknu/backend/web/CrawlingController.java
@@ -1,6 +1,7 @@
 package com.allknu.backend.web;
 
 
+import com.allknu.backend.core.types.MajorNoticeType;
 import com.allknu.backend.core.types.UnivNoticeType;
 import com.allknu.backend.provider.service.CrawlingService;
 import com.allknu.backend.web.dto.CommonResponse;
@@ -10,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,13 +21,30 @@ public class CrawlingController {
     private final CrawlingService crawlingService;
 
     @GetMapping("/crawling/notice/univ/{page}") // 학교 공지사항 크롤링 요청
-    public ResponseEntity<CommonResponse> requestPushNotificationToWeb(@PathVariable int page, @RequestParam(value = "type", required = false, defaultValue = "ALL") UnivNoticeType type) {
+    public ResponseEntity<CommonResponse> getUnivNotice(@PathVariable int page, @RequestParam(value = "type", required = false, defaultValue = "ALL") UnivNoticeType type) {
         List<ResponseCrawling.UnivNotice> notices = crawlingService.getUnivNotice(page, type).orElseGet(() -> null);
 
         CommonResponse response = CommonResponse.builder()
                 .status(HttpStatus.OK.value())
                 .message("성공")
                 .list(notices)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/crawling/notice/major")
+    public ResponseEntity<CommonResponse> getMajorNotice(@RequestParam(required = false, defaultValue = "1") int page, @RequestParam(value = "type", required = true, defaultValue = "SOFTWARE") MajorNoticeType type) {
+        //기본 강남대 템플릿을 사용하는 학과 공지사항을 크롤링해 반환한다. 만약 기본 템플릿을 사용하지 않는 학과가 생긴다면 switch로 학과에 따라 분기해 긁어오는 코드를 작성할 것
+        List<ResponseCrawling.UnivNotice> notices = crawlingService.getMajorDefaultTemplateNotice(page, type).orElseGet(()->null);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("korean", type.getKorean());
+        map.put("notices", notices);
+
+        CommonResponse response = CommonResponse.builder()
+                .status(HttpStatus.OK.value())
+                .message("성공")
+                .list(map)
                 .build();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
