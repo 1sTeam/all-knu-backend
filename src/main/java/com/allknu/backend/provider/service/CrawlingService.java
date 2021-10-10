bf7e3a14206863293bea8b5c251e954edb2ba0a0
suhongkim98 <kshz9815@naver.com>
2021-10-10 16:22:23 +0900

    학과 공지사항 조회 API 제작

--- src/main/java/com/allknu/backend/provider/service/CrawlingService.java
+++ src/main/java/com/allknu/backend/provider/service/CrawlingService.java
@@ -1,6 +1,7 @@
 package com.allknu.backend.provider.service;
 
 import com.allknu.backend.core.service.CrawlingServiceInterface;
+import com.allknu.backend.core.types.MajorNoticeType;
 import com.allknu.backend.core.types.UnivNoticeType;
 import com.allknu.backend.web.dto.ResponseCrawling;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -81,6 +82,92 @@ public class CrawlingService implements CrawlingServiceInterface {
             System.out.println(e);
         }
 
+        return Optional.ofNullable(lists);
+    }
+
+    @Override
+    public Optional<List<ResponseCrawling.UnivNotice>> getMajorDefaultTemplateNotice(int pageNum, MajorNoticeType type) {
+        List<ResponseCrawling.UnivNotice> lists = new ArrayList<>();
+        String url = type.getUrl() + "?paginationInfo.currentPageNo=" + pageNum;
+
+        try {
+            Document doc = Jsoup.connect(url).get();
+
+            //구분이 있는 학과 공지도 있고 구분이 없는 학과 공지가 있다.
+            int tableHeadSize = doc.select("div.thead li").size(); // 테이블 헤드 길이 계산, 6개면 세부구분이 없는 학과
+
+            Iterator<Element> rows = doc.select("div.tbody > ul").iterator();
+            while(rows.hasNext()) {
+                Element target = rows.next();
+                Elements li = target.select("li"); // ul 안의 li들
+
+                String number = li.get(0).text(); // 게시글번호 li
+                if(!StringUtil.isNumeric(number)) {
+                    //넘버가 숫자가 아니라면 필독공지임 이거는 패스
+                    continue;
+                }
+
+                Element linkElement = li.get(1).selectFirst("a.detailLink"); // 링크li
+                JsonNode jsonNode = objectMapper.readTree(linkElement.attr("data-params"));
+                String encMenuSeq = jsonNode.get("encMenuSeq").asText();
+                String encMenuBoardSeq = jsonNode.get("encMenuBoardSeq").asText();
+                String link = type.getBoardUrl() + "?encMenuSeq=" + encMenuSeq + "&encMenuBoardSeq=" + encMenuBoardSeq;
+
+                ResponseCrawling.UnivNotice notice = null;
+                if(tableHeadSize == 7) {
+                    //소응 처럼 세부 구분이 있는 학과 공지사항
+                    String title = linkElement.text();
+                    String writeType = li.get(2).text(); //구분 li
+                    String writer = li.get(4).text(); // 작성자
+                    String date = li.get(5).text(); // date
+                    String views = li.get(6).text(); // views
+
+                    notice = ResponseCrawling.UnivNotice.builder()
+                            .link(link)
+                            .date(date)
+                            .number(number)
+                            .writer(writer)
+                            .type(writeType)
+                            .views(views)
+                            .title(title)
+                            .build();
+                } else if(tableHeadSize == 5) {
+                    //글경은 5개..ㅎㅎ
+                    String title = linkElement.text();
+                    String views = li.get(2).text(); // views
+                    String writer = li.get(3).text(); // 작성자
+                    String date = li.get(4).text(); // date
+
+                    notice = ResponseCrawling.UnivNotice.builder()
+                            .link(link)
+                            .date(date)
+                            .number(number)
+                            .writer(writer)
+                            .views(views)
+                            .title(title)
+                            .build();
+                } else {
+                    //교육학과처럼 세부 구분이 없는 학과 공지사항
+                    String title = linkElement.text();
+                    String writer = li.get(3).text(); // 작성자
+                    String date = li.get(4).text(); // date
+                    String views = li.get(5).text(); // views
+
+                    notice = ResponseCrawling.UnivNotice.builder()
+                            .link(link)
+                            .date(date)
+                            .number(number)
+                            .writer(writer)
+                            .views(views)
+                            .title(title)
+                            .build();
+                }
+                lists.add(notice);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
         return Optional.ofNullable(lists);
     }
 }
