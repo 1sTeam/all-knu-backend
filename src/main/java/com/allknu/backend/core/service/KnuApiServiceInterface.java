d85782d15149bb1ef20063cb20267659e89a0245
suhongkim98 <kshz9815@naver.com>
2021-10-27 11:10:26 +0900

    시간표 조회API 구현

--- src/main/java/com/allknu/backend/core/service/KnuApiServiceInterface.java
+++ src/main/java/com/allknu/backend/core/service/KnuApiServiceInterface.java
@@ -1,9 +1,13 @@
 package com.allknu.backend.core.service;
 
+import com.allknu.backend.web.dto.RequestKnu;
+import com.allknu.backend.web.dto.ResponseKnu;
+
 import java.util.Map;
 import java.util.Optional;
 
 public interface KnuApiServiceInterface {
     Optional<Map<String, String>> login(String id, String password);
     void logout(Map<String, String> cookies);
+    Optional<ResponseKnu.TimeTable> getTimeTable(Map<String, String> cookies);
 }
