80478a9a48fa931dafe9a51274bb59211b83b8df
suhongkim98 <kshz9815@naver.com>
2021-10-29 15:35:04 +0900

    재학기간 조회API, 성적표 조회 API 작성

--- src/main/java/com/allknu/backend/core/service/KnuApiServiceInterface.java
+++ src/main/java/com/allknu/backend/core/service/KnuApiServiceInterface.java
@@ -2,12 +2,16 @@ package com.allknu.backend.core.service;
 
 import com.allknu.backend.web.dto.RequestKnu;
 import com.allknu.backend.web.dto.ResponseKnu;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.Map;
 import java.util.Optional;
 
 public interface KnuApiServiceInterface {
+    Optional<JsonNode> getKnuApiJsonData(String url, Map<String,String> cookies);
     Optional<Map<String, String>> login(String id, String password);
     void logout(Map<String, String> cookies);
     Optional<ResponseKnu.TimeTable> getTimeTable(Map<String, String> cookies);
+    Optional<ResponseKnu.PeriodUniv> getPeriodOfUniv(Map<String, String> cookies);
+    Optional<ResponseKnu.Grade> getGrade(Map<String, String> cookies, String year, String semester);
 }
