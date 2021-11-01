9b0bbd30136c573bc443f6730575848e9d167d91
suhongkim98 <kshz9815@naver.com>
2021-11-01 18:00:55 +0900

    메시지 전송 시 로그 등록, 로그 조회 API 작성

--- src/main/java/com/allknu/backend/provider/service/FCMApiService.java
+++ src/main/java/com/allknu/backend/provider/service/FCMApiService.java
@@ -1,27 +1,53 @@
 package com.allknu.backend.provider.service;
 
 import com.allknu.backend.core.service.FCMApiServiceInterface;
+import com.allknu.backend.entity.FirebaseLog;
 import com.allknu.backend.kafka.MessageProducer;
 import com.allknu.backend.kafka.dto.FCMSubscribeMessage;
 import com.allknu.backend.kafka.dto.FCMWebMessage;
+import com.allknu.backend.repository.FirebaseLogRepository;
 import com.allknu.backend.web.dto.RequestFCMMessage;
+import com.allknu.backend.web.dto.ResponseFcm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FCMApiService implements FCMApiServiceInterface {
     private final MessageProducer messageProducer; // FCM 마이크로서비스로 보내기 위한 카프카 프로듀서
+    private final FirebaseLogRepository firebaseLogRepository;
 
+    @Transactional
     @Override
-    public void pushToKafkaWebMessage(RequestFCMMessage.Web message) {
+    public void pushToKafkaWebMessage(String email, RequestFCMMessage.Web message) {
         FCMWebMessage fcmWebMessage = message.toFCMRequestMessage();
         messageProducer.sendFCMMessage(fcmWebMessage);
+
+        //log 등록
+        FirebaseLog log = FirebaseLog.builder()
+                .adminEmail(email)
+                .title(message.getTitle())
+                .body(message.getBody())
+                .link(message.getClickLink())
+                .build();
+        firebaseLogRepository.save(log);
     }
 
     @Override
     public void pushToKafkaSubscribeMessage(RequestFCMMessage.Subscribe message) {
         FCMSubscribeMessage fcmSubscribeMessage = message.toFCMSubscribeMessage();
         messageProducer.sendFCMRequestSubscribeMessage(fcmSubscribeMessage);
+    }
+
+    @Transactional
+    @Override
+    public Optional<Page<ResponseFcm.Log>> getAllFcmLog(Pageable pageable) {
+        Page<FirebaseLog> logs = firebaseLogRepository.findAll(pageable);
+        return Optional.ofNullable(logs.map(ResponseFcm.Log::of));
     }
 }
