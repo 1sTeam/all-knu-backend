1419976a4f20f720fdf9ac46dfc020bab422a487
suhongkim98 <kshz9815@naver.com>
2021-11-01 18:00:55 +0900

    메시지 전송 시 로그 등록, 로그 조회 API 작성

--- src/main/java/com/allknu/backend/core/service/FCMApiServiceInterface.java
+++ src/main/java/com/allknu/backend/core/service/FCMApiServiceInterface.java
@@ -1,8 +1,14 @@
 package com.allknu.backend.core.service;
 
 import com.allknu.backend.web.dto.RequestFCMMessage;
+import com.allknu.backend.web.dto.ResponseFcm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
 
 public interface FCMApiServiceInterface {
-    void pushToKafkaWebMessage(RequestFCMMessage.Web message);
+    void pushToKafkaWebMessage(String email, RequestFCMMessage.Web message);
     void pushToKafkaSubscribeMessage(RequestFCMMessage.Subscribe message);
+    Optional<Page<ResponseFcm.Log>> getAllFcmLog(Pageable pageable);
 }
