943ef36b1edd844e598347c974b508b088d0fe98
suhongkim98 <kshz9815@naver.com>
2021-11-12 15:11:15 +0900

    컨슈머 구현

--- src/main/java/com/allknu/backend/kafka/MessageConsumer.java
+++ src/main/java/com/allknu/backend/kafka/MessageConsumer.java
@@ -1,16 +1,32 @@
 package com.allknu.backend.kafka;
 
 
+import com.allknu.backend.kafka.dto.FCMWebMessage;
+import com.allknu.backend.kafka.dto.MLRequestMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class MessageConsumer {
+    private final MessageProducer producer;
 
-    //@KafkaListener(topics = "exam", groupId = "foo")
-    //public void consume(String message) throws IOException {
-    //    System.out.println(String.format("Consumed message : %s", message));
-    //}
+    @KafkaListener(topics = "mlRequest", groupId = "all-knu-backend", containerFactory = "MLRequestMessageListener")
+    public void consume(@Payload MLRequestMessage message, @Headers MessageHeaders headers) throws IOException {
+        System.out.println(String.format("Consumed message : %s", message.getTitle()));
+        FCMWebMessage fcmWebMessage = FCMWebMessage.builder()
+                .title(message.getTitle())
+                .clickLink(message.getClickLink())
+                .body("비교과 프로그램 안내")
+                .subscribeTypes(message.getSubscribeTypes())
+                .build();
+
+        producer.sendFCMMessage(fcmWebMessage);
+    }
 }
