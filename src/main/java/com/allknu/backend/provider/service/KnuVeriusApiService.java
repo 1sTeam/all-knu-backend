5ede6fd8a16a3fdaf2f6024d705dcb324ceb8c0a
suhongkim98 <kshz9815@naver.com>
2021-11-17 18:47:17 +0900

    sso 토큰 이용한 참인재 크롤링 및 로그인 api 수정

---
+++ src/main/java/com/allknu/backend/provider/service/KnuVeriusApiService.java
@@ -0,0 +1,45 @@
+package com.allknu.backend.provider.service;
+
+import com.allknu.backend.core.service.KnuVeriusApiServiceInterface;
+import com.allknu.backend.exception.errors.KnuApiCallFailedException;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+//참인재시스템 verius 오타 아님 ㅎㅎ
+public class KnuVeriusApiService implements KnuVeriusApiServiceInterface {
+    @Override
+    public Optional<Map<String, String>> getStudentInfo(Map<String, String> ssoCookies) {
+        //참인재시스템에서 학과, 학번, 이름 등 학생 정보를 긁어다 준다.
+        String url = "https://verius.kangnam.ac.kr/user/Std/MyHm010.do?CURRENT_MENU_CODE=MENU0264&TOP_MENU_CODE=MENU0010";
+        Map<String, String> result;
+        try {
+            Document res = Jsoup.connect(url)
+                    .method(Connection.Method.GET)
+                    .cookies(ssoCookies)
+                    .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36")
+                    .get();
+
+            Elements tableViews = res.select("div.tableView tbody"); // tableView tbody를 리스트로 가져온다.
+            Elements dataList = tableViews.get(0).select("td"); // 학생 기본정보 테이블의 td들
+
+            result = new HashMap<>();
+            result.put("name", dataList.get(0).text());
+            result.put("id", dataList.get(1).text());
+            result.put("major", dataList.get(3).text());
+
+        } catch (IOException e) {
+            System.out.println(e);
+            throw new KnuApiCallFailedException();
+        }
+        return Optional.ofNullable(result);
+    }
+}
