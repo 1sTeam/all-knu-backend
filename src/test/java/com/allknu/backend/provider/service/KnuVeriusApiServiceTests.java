4bb0ebcbc2bf7eeed61c61e43f373d5f20413d1b
suhongkim98 <kshz9815@naver.com>
2021-11-30 22:33:10 +0900

    만족도조사 테스트코드 작성

--- src/test/java/com/allknu/backend/provider/service/KnuVeriusApiServiceTests.java
+++ src/test/java/com/allknu/backend/provider/service/KnuVeriusApiServiceTests.java
@@ -1,11 +1,13 @@
 package com.allknu.backend.provider.service;
 
+import com.allknu.backend.web.dto.ResponseKnu;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -34,6 +36,21 @@ public class KnuVeriusApiServiceTests {
         System.out.println("정보출력");
         for( Map.Entry<String, String> elem : info.entrySet() ){
             System.out.println( String.format("키 : %s, 값 : %s", elem.getKey(), elem.getValue()) );
+        }
+    }
+    @Test
+    @DisplayName("만족도 조사 참여 현황 테스트")
+    void getSatisfactionTest() {
+        // sso login
+        Map<String, String> cookies = knuApiService.ssoLogin("1234","1234").orElseGet(()->null);
+        assertNotNull(cookies);
+
+        //get student info
+        List<ResponseKnu.VeriusSatisfaction> list = knuVeriusApiService.getMyVeriusSatisfactionInfo(cookies, 1).orElseGet(()->null);
+        assertNotNull(list);
+        System.out.println("정보출력");
+        for( ResponseKnu.VeriusSatisfaction elem : list ){
+            System.out.println(elem.getName()+" "+elem.getNumber()+" "+elem.getSatisfactionEndDate()+" "+elem.getOperationEndDate()+" "+elem.getStatus());
         }
     }
 }
