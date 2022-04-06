840e11921307656d38814894c300ab2004674aab
suhongkim98 <kshz9815@naver.com>
2022-04-06 15:03:27 +0900

    flyway properties 작성 및 init table 스키마 작성

---
+++ src/main/resources/db/migration/V202204061__init_table.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `fcm_log`;
+
+CREATE TABLE `fcm_log` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `admin_email` VARCHAR(100),
+    `title` VARCHAR(100),
+    `body` VARCHAR(100),
+    `link` VARCHAR(255),
+    `timestamp` DATETIME
+);
