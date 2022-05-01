8d71e99a68ab81e31b4a9ce95571c465a35080a8
suhongkim98 <kshz9815@naver.com>
2022-05-01 21:50:58 +0900

    private policy 응답 정적페이지 구현

--- src/main/java/com/allknu/backend/web/WebController.java
+++ src/main/java/com/allknu/backend/web/WebController.java
@@ -3,14 +3,21 @@ package com.allknu.backend.web;
 import com.allknu.backend.web.dto.CommonResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class WebController {
 
     @GetMapping("/dev/health-check")
+    @ResponseBody
     public ResponseEntity<CommonResponse> healthCheck() {
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping("/dev/policy")
+    public String policy() {
+        return "policy";
     }
 }
