e3ed756035d69430cc783abf3c17bfdbf36621fc
suhongkim98 <kshz9815@naver.com>
2022-05-05 19:29:36 +0900

    fix: 학과를 찾지 못하면 default로 SOFTWARE 지정

---
+++ src/test/java/com/allknu/backend/core/types/MajorNoticeTypeTests.java
@@ -0,0 +1,32 @@
+package com.allknu.backend.core.types;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MajorNoticeTypeTests {
+
+    @Test
+    @DisplayName("학과를 찾는다.")
+    void findMajorTest() {
+        String target = "소프트웨어응용학부";
+
+        MajorNoticeType type = MajorNoticeType.findByMajor(target);
+        assertEquals(type, MajorNoticeType.SOFTWARE);
+
+        target = "글로벌경영학부";
+        type = MajorNoticeType.findByMajor(target);
+        assertEquals(type, MajorNoticeType.GLOBAL_BIZ);
+    }
+
+    @Test
+    @DisplayName("학과를 찾지 못하면 디폴트로 SOFTWARE을 준다.")
+    void ifNotFindMajorThenGiveSOFTWARETest() {
+        String target = "난없어학과";
+
+        MajorNoticeType type = MajorNoticeType.findByMajor(target);
+        assertEquals(type, MajorNoticeType.SOFTWARE);
+    }
+}
