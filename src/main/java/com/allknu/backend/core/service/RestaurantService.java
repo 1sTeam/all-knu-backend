13e2659611f5039543b8204d0aa6aa277ae12bde
suhongkim98 <kshz9815@naver.com>
2022-05-11 01:08:39 +0900

    feature: 식단표 조회 API 구현(테스트코드 작성 필요)

--- src/main/java/com/allknu/backend/core/service/RestaurantService.java
+++ src/main/java/com/allknu/backend/core/service/RestaurantService.java
@@ -2,6 +2,7 @@ package com.allknu.backend.core.service;
 
 
 import com.allknu.backend.core.types.MealType;
+import com.allknu.backend.web.dto.ResponseRestaurant;
 
 import java.util.Date;
 import java.util.List;
@@ -10,4 +11,5 @@ public interface RestaurantService {
    void registerRestaurant(String restaurant);
    void registerMenu(String restaurant, Date date, List<String> menu, MealType time);
    List<String> getAllRestaurants();
+   List<ResponseRestaurant.FindMenu> getAllMenuByDate(Date date);
 }
--- src/main/java/com/allknu/backend/repository/MenuRepository.java
+++ src/main/java/com/allknu/backend/repository/MenuRepository.java
@@ -1,9 +1,20 @@
 package com.allknu.backend.repository;
 
+import com.allknu.backend.core.types.MealType;
 import com.allknu.backend.entity.Menu;
 import com.allknu.backend.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     Menu findByMenuName(String menuName);
+
+    @Query("select m from Menu m " +
+            "join m.restaurant r " +
+            "where r.restaurantName = :restaurantName " +
+            "and m.mealDate = :date")
+    List<Menu> findAllByMealTypeAndDateAndRestaurant(String restaurantName, Date date);
 }
