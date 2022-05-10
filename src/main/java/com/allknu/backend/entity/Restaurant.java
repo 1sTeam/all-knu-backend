d1c6023b43917c87a02588a79e98a61317d36a1d
skfkgla <79778428+skfkgla@users.noreply.github.com>
2022-05-11 00:26:23 +0900

    [feat] (#65) 학식 등록 API, 식당 등록 API 구현
    * [feat]
    학식 엔티티 설계 및 RepositoryTest
    
    * [feat]
    학식 메뉴 및 식당 등록 개발

---
+++ src/main/java/com/allknu/backend/entity/Restaurant.java
@@ -0,0 +1,29 @@
+package com.allknu.backend.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name="restaurant")
+@Entity
+@Getter
+@NoArgsConstructor
+public class Restaurant {
+    @Id
+    @Column(name = "restaurant_name")
+    private String restaurantName;
+
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
+    private List<Menu> menuList = new ArrayList<>();
+
+    @Builder
+    public Restaurant(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+
+}
