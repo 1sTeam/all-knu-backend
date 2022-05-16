840840b451f352dc1d27ae16b3e7231642b76535
suhongkim98 <kshz9815@naver.com>
2022-05-16 21:50:43 +0900

    fix: 관리자페이지에서 DELETE에 대해 CORS뜨던 문제 해결

--- src/main/java/com/allknu/backend/configuration/WebConfig.java
+++ src/main/java/com/allknu/backend/configuration/WebConfig.java
@@ -1,6 +1,7 @@
 package com.allknu.backend.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*") //cors 문제 해결
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name()
+                );
     }
 }
