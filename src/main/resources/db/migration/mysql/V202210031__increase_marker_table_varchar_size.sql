93455863a45a4854624da69fb9792eb9d8075391
suhongkim98 <kshz9815@naver.com>
2022-10-03 15:20:28 +0900

    fix: INPLACE algorithm not support

--- src/main/resources/db/migration/mysql/V202210031__increase_marker_table_varchar_size.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE map_marker_operation_info MODIFY operation_time VARCHAR(127), algorithm=inplace, lock = none;
-ALTER TABLE map_marker_operation_info MODIFY phone VARCHAR(255), algorithm=inplace, lock = none;
