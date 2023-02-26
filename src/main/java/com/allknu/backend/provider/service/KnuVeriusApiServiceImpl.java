1d94695a1a94809ade6ef702083493bdfaff459a
suhongkim98 <kshz9815@naver.com>
2023-02-26 19:54:39 +0900

    fix : verius 참인재 마이페이지 주소 변경에 따른 학생 정보 조회 실패 문제 해결

--- src/main/java/com/allknu/backend/provider/service/KnuVeriusApiServiceImpl.java
+++ src/main/java/com/allknu/backend/provider/service/KnuVeriusApiServiceImpl.java
@@ -61,7 +61,7 @@ public class KnuVeriusApiServiceImpl implements KnuVeriusApiService {
     public Optional<Map<String, String>> getStudentInfo(Map<String, String> veriusCookies) {
         //참인재시스템에서 학과, 학번, 이름 등 학생 정보를 긁어다 준다.
         //해당 참인재 쿠키로 정보를 긁어온다.
-        String url = "https://verius.kangnam.ac.kr/user/Std/MyHm010.do?CURRENT_MENU_CODE=MENU0264&TOP_MENU_CODE=MENU0010";
+        String url = "https://verius.kangnam.ac.kr/user/Std/MyHm010.do?CURRENT_MENU_CODE=MENU0028&TOP_MENU_CODE=MENU0017";
         Map<String, String> result = null;
         try {
             Document res = Jsoup.connect(url)
@@ -82,7 +82,7 @@ public class KnuVeriusApiServiceImpl implements KnuVeriusApiService {
             logger.error("getStudentInfo IOException error " + e);
             throw new KnuApiCallFailedException();
         } catch (IndexOutOfBoundsException e) {
-            logger.info("세션이 유효하지않아 학생정보 조회 실패 " + e);
+            logger.error("세션이 유효하지않아 학생정보 조회 실패 " + e);
             throw new KnuApiCallFailedException();
         } catch (Exception e) {
             logger.error("학생정보 조회 시 알수없는 에러 " + e);
