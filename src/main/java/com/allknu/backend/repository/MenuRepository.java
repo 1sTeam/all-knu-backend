b3f6ca4617f91b7071f1d5a657ea661cc43f9e8f
suhongkim98 <kshz9815@naver.com>
2023-09-21 08:17:03 +0900

    fix : InvalidDataAccessApiUsageException

--- src/main/java/com/allknu/backend/repository/MenuRepository.java
+++ src/main/java/com/allknu/backend/repository/MenuRepository.java
@@ -5,6 +5,7 @@ import com.allknu.backend.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
@@ -18,16 +19,16 @@ public interface MenuRepository extends JpaRepository<Menu, Long> {
             "join m.restaurant r " +
             "where r.restaurantName = :restaurantName " +
             "and m.mealDate = :date")
-    List<Menu> findAllByDateAndRestaurant(String restaurantName, Date date);
+    List<Menu> findAllByDateAndRestaurant(@Param("restaurantName") String restaurantName, @Param("date") Date date);
 
     @Query("select m from Menu m " +
             "join m.restaurant r " +
             "where r.restaurantName = :restaurantName " +
             "and m.mealDate = :date " +
             "and m.mealType = :type")
-    List<Menu> findByRestaurantAndDateAndType(String restaurantName, Date date, MealType type);
+    List<Menu> findByRestaurantAndDateAndType(@Param("restaurantName") String restaurantName, @Param("date") Date date, @Param("type") MealType type);
     @Query("delete from Menu m " +
             "where m in(:menuList)")
     @Modifying
-    void deleteMenuList(List<Menu> menuList);
+    void deleteMenuList(@Param("menuList") List<Menu> menuList);
 }
