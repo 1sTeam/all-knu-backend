beb7d12f866d5bd8b954c934d9f63e642b43d786
suhongkim98 <kshz9815@naver.com>
2023-09-28 19:16:56 +0900

    refactor : 레거시 정류장 시간표 API 삭제 작업

--- src/main/java/com/allknu/backend/core/service/ShuttleService.java
+++ src/main/java/com/allknu/backend/core/service/ShuttleService.java
@@ -9,9 +9,7 @@ public interface ShuttleService {
     void registerStation(String stationName);
     List<String> getAllStation();
     void deleteStation(String stationName);
-    void registerStationTimetable(String stationName, Date stopTime);
     void registerStationTimetable(String stationName, Date stopTime, String destination);
-    List<ResponseStation.StationTime> getAllStationTimetable();
     List<ResponseStation.getStationTime> getStationTimetable();
     void deleteStationTimetable(String stationName, Date stopTime);
 }
